3159bfb61be2a2f86b1c99b822c491d742430f2b
Mason <asonmay09@gmail.com>
2024-01-04 21:02:58 -0800

    Add project files.

---
+++ CvPlatformer/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿
+namespace CvPlatformer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(557, 315);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(551, 309);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(13, 358);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(557, 306);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(277, 155);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(269, 144);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 3;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Location = new System.Drawing.Point(3, 155);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(268, 144);
+            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox5.TabIndex = 2;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(277, 5);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(269, 144);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(268, 144);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 32;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.TabStop = false;
+            this.button1.Text = "save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(97, 332);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.TabStop = false;
+            this.button2.Text = "update";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 676);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "   ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ CvPlatformer/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+
+namespace CvPlatformer
+{
+    public partial class Form1 : Form
+    {
+        Mat currentFrame;
+        VideoCapture capture;
+        List<Rectangle> platforms;
+        Vector2 playerPosition;
+        Vector2 speed;
+        Size playerSize;
+        Mat saveFrame = new Mat();
+        bool isUpdateing = true;
+        VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(isUpdateing)
+            {
+                currentFrame = capture.QueryFrame();
+                pictureBox3.Image = capture.QueryFrame().ToBitmap();
+                perspectiveTransform();
+                trackPlatforms();
+            }
+            else
+            {
+                player();
+            }
+            draw();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            capture = new VideoCapture(0);
+            playerPosition = new Vector2(100, 100);
+            speed = new Vector2(0, 0);
+            playerSize = new Size(30, 40);
+        }
+        public void player()
+        {
+            playerPosition.Y += speed.Y;
+            playerPosition.X += speed.X;
+            speed.Y += 2f;
+            for(int i = 0; i < platforms.Count; i++)
+            {
+                if (new Rectangle(new Point((int)playerPosition.X, (int)playerPosition.Y), playerSize).IntersectsWith(platforms[i]))
+                {
+                    speed.Y = -2.3f;  
+                }
+            }         
+            if(playerPosition.Y > 500)
+            {
+                playerPosition.Y = 50;
+                speed.Y = 0;
+            }
+        }
+        private void perspectiveTransform()
+        {
+            Mat image = currentFrame;
+            Mat output = image.Clone();
+            Mat imageClone = image.Clone();
+            CvInvoke.GaussianBlur(imageClone, imageClone, new Size(7, 7), 0);
+            CvInvoke.CvtColor(imageClone, imageClone, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+            CvInvoke.InRange(imageClone, (ScalarArray)new MCvScalar(75, 0, 127), (ScalarArray)new MCvScalar(179, 65, 255), imageClone);
+            pictureBox4.Image = imageClone.ToBitmap();
+            contours = new VectorOfVectorOfPoint();
+            Mat heirachy = new Mat();
+            CvInvoke.FindContours(imageClone, contours, heirachy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);
+
+            int index = 0;
+            var biggestContourArea = double.MinValue;
+            for (int i = 0; i < contours.Size; i++)
+            {
+                double area = CvInvoke.ContourArea(contours[i]);
+                if (biggestContourArea <= area)
+                {
+                    index = i;
+                    biggestContourArea = area;
+                }
+            }
+
+            if (contours.Size == 0 || biggestContourArea < 500)
+            {
+                return;
+            }
+
+            CvInvoke.ApproxPolyDP(contours[index], contours[index], 0.02 * CvInvoke.ArcLength(contours[index], true), true);
+            CvInvoke.DrawContours(image, contours, -1, new MCvScalar(255, 0, 0), 4);
+            pictureBox5.Image = image.ToBitmap();
+            PointF[] dest = new PointF[]
+            {
+                new PointF(0,0),
+                new PointF(output.Width,0),
+                new PointF(output.Width,output.Height),
+                new PointF(0,output.Height),
+            };
+
+            CvInvoke.WarpPerspective(output, output, CvInvoke.GetPerspectiveTransform(new PointF[] { contours[index][0], contours[index][1], contours[index][2], contours[index][3] }, dest), output.Size);
+            CvInvoke.Flip(output, output, Emgu.CV.CvEnum.FlipType.Horizontal);
+            pictureBox2.Image = output.ToBitmap();
+            pictureBox2.Tag = output;
+        }
+
+        private void trackPlatforms()
+        {
+            Mat image = (Mat)pictureBox2.Tag;
+            Mat imageClone = image.Clone();
+            CvInvoke.GaussianBlur(imageClone, imageClone, new Size(5, 5), 0);
+            CvInvoke.CvtColor(imageClone, imageClone, Emgu.CV.CvEnum.ColorConversion.Bgr2Hsv);
+            CvInvoke.InRange(imageClone, (ScalarArray)new MCvScalar(0, 0, 0), (ScalarArray)new MCvScalar(255, 255, 60), imageClone);
+            
+            Mat heirachy = new Mat();
+            CvInvoke.FindContours(imageClone, contours, heirachy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxNone);
+            platforms = new List<Rectangle>();
+            for(int i = 0; i < contours.Size; i++)
+            {
+                if (CvInvoke.ContourArea(contours[i]) > 50)
+                {
+                    platforms.Add(CvInvoke.BoundingRectangle(contours[i]));                  
+                }
+            }
+            
+        }
+        private void draw()
+        {
+            Mat image = (Mat)pictureBox2.Tag;
+            Mat output = image.Clone();
+            for(int i = 0; i < platforms.Count; i++)
+            {
+                CvInvoke.Rectangle(output, CvInvoke.BoundingRectangle(contours[i]), new MCvScalar(0, 255, 255), -1);
+            }
+            CvInvoke.Rectangle(output, new Rectangle((int)playerPosition.X, (int)playerPosition.Y, playerSize.Width, playerSize.Height), new MCvScalar(255, 0, 0), -1);
+            pictureBox1.Image = output.ToBitmap();
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyData == Keys.Up)
+            {
+                speed = new Vector2(speed.X,-25);
+            }
+
+            if(e.KeyData == Keys.Right)
+            {
+                speed.X = 12;
+            }
+            else if(e.KeyData == Keys.Left)
+            {
+                speed.X = -12;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            speed.X = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            isUpdateing = false;
+            saveFrame = currentFrame;
+
+            pictureBox1.Focus();
+        }
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            isUpdateing = true;
+            pictureBox1.Focus();
+        }
+    }
+}
